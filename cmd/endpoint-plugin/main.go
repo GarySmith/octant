bbe12504eaa207503724d91a2236221e0d340c46
Gary Smith <gary.smith@suse.com>
2019-09-12 15:27:37 -0700

    Add support for openstack servers list

--- cmd/endpoint-plugin/main.go
+++ cmd/endpoint-plugin/main.go
@@ -60,6 +60,11 @@ func handleNavigation(request *service.NavigationRequest) (navigation.Navigation
 				Path:     request.GeneratePath("endpoints"),
 				IconName: "folder",
 			},
+			{
+				Title:    "Servers",
+				Path:     request.GeneratePath("servers"),
+				IconName: "folder",
+			},
 		},
 		IconName: "cloud",
 	}, nil
@@ -75,51 +80,101 @@ type endpoint struct {
 	ID          string
 }
 
+type server struct {
+	Status      string
+	Name        string
+	Image       string
+	ID          string
+	Flavor      string
+	Networks    string
+}
+
+
+func endpointList(request *service.Request) (component.ContentResponse, error) {
+
+    // Requires that you have appropriate OS_* variables defined in the environment so that 'openstack endpoint list' succeeds
+    out, err := exec.Command("openstack", "endpoint", "list", "-f", "json").Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Convert output into an array slice of endpoint structs.
+    res := []endpoint{}
+    if err := json.Unmarshal(out, &res); err != nil {
+        empty := component.ContentResponse{}
+        return empty, err
+    }
+
+    // Sort the table by service type and interface
+    sort.Slice(res, func(i, j int) bool {
+        if res[i].ServiceType < res[j].ServiceType {
+            return true
+        } else if res[i].ServiceType > res[j].ServiceType {
+            return false
+        }
+        return res[i].Interface < res[j].Interface
+    })
+
+    cols := component.NewTableCols("ServiceType", "URL", "Region", "Enabled", "Interface", "ServiceName")
+    table := component.NewTable("Endpoints", "placeholder", cols)
+    for _, elem := range res {
+        row := component.TableRow{}
+        row["ServiceType"] = component.NewText(elem.ServiceType)
+        row["URL"] = component.NewText(elem.URL)
+        row["Region"] = component.NewText(elem.Region)
+        row["Enabled"] = component.NewText(strconv.FormatBool(elem.Enabled))
+        row["Interface"] = component.NewText(elem.Interface)
+        row["ServiceName"] = component.NewText(elem.ServiceName)
+        table.Add(row)
+    }
+
+    contentResponse := component.NewContentResponse(component.TitleFromString("Endpoints"))
+    contentResponse.Add(table)
+    return *contentResponse, nil
+}
+
+func serverList(request *service.Request) (component.ContentResponse, error) {
+
+    // Requires that you have appropriate OS_* variables defined in the environment so that 'openstack servers list' succeeds
+    out, err := exec.Command("openstack", "server", "list", "-f", "json").Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Convert output into an array slice of server structs.
+    res := []server{}
+    if err := json.Unmarshal(out, &res); err != nil {
+        empty := component.ContentResponse{}
+        return empty, err
+    }
+
+    // Sort the table by service type and interface
+    sort.Slice(res, func(i, j int) bool {
+        return res[i].Name < res[j].Name
+    })
+
+    cols := component.NewTableCols("Name", "ID", "Status", "Image", "Flavor", "Networks")
+    table := component.NewTable("Servers", "placeholder", cols)
+    for _, elem := range res {
+        row := component.TableRow{}
+        row["Name"] = component.NewText(elem.Name)
+        row["ID"] = component.NewText(elem.ID)
+        row["Status"] = component.NewText(elem.Status)
+        row["Image"] = component.NewText(elem.Image)
+        row["Flavor"] = component.NewText(elem.Flavor)
+        row["Networks"] = component.NewText(elem.Networks)
+        table.Add(row)
+    }
+
+    contentResponse := component.NewContentResponse(component.TitleFromString("Servers"))
+    contentResponse.Add(table)
+    return *contentResponse, nil
+}
+
 // initRoutes routes for this plugin. In this example, there is a global catch all route
 // that will return the content for every single path.
 func initRoutes(router *service.Router) {
 
-	router.HandleFunc("/*", func(request *service.Request) (component.ContentResponse, error) {
-
-		// Requires that you have appropriate OS_* variables defined in the environment so that 'openstack endpoint list' succeeds
-		out, err := exec.Command("openstack", "endpoint", "list", "-f", "json").Output()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Convert output into an array slice of endpoint structs.
-		res := []endpoint{}
-		if err := json.Unmarshal(out, &res); err != nil {
-			empty := component.ContentResponse{}
-			return empty, err
-		}
-
-		// Sort the table by service type and interface
-		sort.Slice(res, func(i, j int) bool {
-			if res[i].ServiceType < res[j].ServiceType {
-				return true
-			} else if res[i].ServiceType > res[j].ServiceType {
-				return false
-			}
-			return res[i].Interface < res[j].Interface
-		})
-
-		cols := component.NewTableCols("ServiceType", "URL", "Region", "Enabled", "Interface", "ServiceName")
-		table := component.NewTable("Endpoints", "placeholder", cols)
-		for _, elem := range res {
-			row := component.TableRow{}
-			row["ServiceType"] = component.NewText(elem.ServiceType)
-			row["URL"] = component.NewText(elem.URL)
-			row["Region"] = component.NewText(elem.Region)
-			row["Enabled"] = component.NewText(strconv.FormatBool(elem.Enabled))
-			row["Interface"] = component.NewText(elem.Interface)
-			row["ServiceName"] = component.NewText(elem.ServiceName)
-			table.Add(row)
-		}
-
-		contentResponse := component.NewContentResponse(component.TitleFromString("Endpoints"))
-		contentResponse.Add(table)
-
-		return *contentResponse, nil
-	})
+	router.HandleFunc("/endpoints", endpointList)
+	router.HandleFunc("/servers", serverList)
 }
